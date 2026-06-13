aef1248556a961968ce796c2d3ff1006024b89d4
Paridhi <paridhibabel239@gmail.com>
2026-06-13 18:11:56 +0530

    fix: resolve dashboard import and firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBDR3gyj6kDyM10-W8Ga0Ei7W6QkKwsFHQ",
+  authDomain: "sdground-556c2.firebaseapp.com",
+  projectId: "sdground-556c2",
+  storageBucket: "sdground-556c2.firebasestorage.app",
+  messagingSenderId: "315874460490",
+  appId:  "1:315874460490:web:d1143376dd626b1bec37b3",
 };
 
 const app = initializeApp(firebaseConfig);
